d243ac41694dce0b141aa1c015cff62d0e2a9dcc
JanhaviByjus <88009471+JanhaviByjus@users.noreply.github.com>
2021-07-27 05:15:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function setup() {
+  createCanvas(400,400);
+ s1= new Student("Adrel",15,99)
+ s1.display();
+
+ s2= new Student("Liam",39,50)
+ s2.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,18 @@
+
+class Student
+{
+   constructor(name,rollno, marks){
+       this.name=name;
+       this.marks=marks;
+       this.rollno= rollno;
+   }
+
+   display(){
+       console.log(this.name)
+       console.log(this.rollno)
+       console.log(this.marks)
+   }
+}
+
+
+
